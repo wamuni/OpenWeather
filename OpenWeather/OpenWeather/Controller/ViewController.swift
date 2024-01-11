ab6b31cf2e9fa370621707194734692172f8774a
wamuni <boshi.pan@outlook.com>
2024-01-11 00:15:59 +0000

    Set up project with MVC pattern

---
+++ OpenWeather/OpenWeather/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by 潘博石 on 11/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
